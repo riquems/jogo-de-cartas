8aa20ac995a53fc2ddaa660e2dc21753c55eb3a0
Henrique Souza <Henrique-MS@Outlook.com>
2022-04-04 14:28:37 -0300

    Add comments

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,9 @@
 #nullable enable
+
+/// <summary>
+/// Classe GameManager, uma forma alternativa para se guardar variáveis globais da aplicação
+/// </summary>
 public static class GameManager
 {
-    public static string? lastGameModeChosen = null;
+    public static string? lastGameModeChosen = null; // Último modo de jogo selecionado
 }
